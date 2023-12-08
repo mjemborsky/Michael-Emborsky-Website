f373bbc5ed5a2265453dbf66e0cb7c53353be8ce
mjemborsky <33811013+mjemborsky@users.noreply.github.com>
2023-12-07 22:17:50 -0500

    nav animation

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -2,6 +2,12 @@
 // Created by: Michael Emborsky
 // Purpose:
 
+
+// TO DO
+// Ideally not have this page at all and do all animations in css
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     // Initializing Properties
     var header = document.querySelector('header');
@@ -10,6 +16,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var links = document.querySelector('.main-link');
     links.style.display = 'none';
     var isExpanded = false; // Flag to track the state of expanded links
+    chevronPointer.style.transformOrigin = 'center';
+    function rotateIn() {
+      chevronPointer.style.transition = 'transform 0.8s ease-in-out'; // Add transition for smooth animation
+      chevronPointer.style.transform = 'rotate(180deg)'; // Rotate to 180 degrees
+    }
+    function rotateOut() {
+      chevronPointer.style.transform = 'rotate(0deg)';
+    }
     // Expand or Collapse Links
     chevron.addEventListener('click', function(e) {
       e.preventDefault();
@@ -19,6 +33,8 @@ document.addEventListener('DOMContentLoaded', function() {
         header.style.width = '0';
         chevron.style.left = '0';
         isExpanded = false;
+        chevronPointer.setAttribute('transform', `rotate(0 25 50)`);
+        rotateOut();
       } else {
         // Expand the links
         links.style.display = 'flex';
@@ -27,7 +43,13 @@ document.addEventListener('DOMContentLoaded', function() {
         header.style.width = '20%';
         chevron.style.left = '20%';
         isExpanded = true;
+        chevronPointer.setAttribute('transform', `rotate(180 25 50)`);
+        rotateIn();
       }
+      header.style.transition = 'width 0.5s ease-in-out';
+      chevron.style.transition = 'left 0.5s ease-in-out';
+      chevronPointer.style.transition = 'transform .5s ease-in-out';
+      links.style.transition = 'display 1s ease-in-out';
     });
   });
   
